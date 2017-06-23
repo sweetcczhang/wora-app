a6794cb791be999ad3a1eadf9972a126162becb1
sweetzcc <sweetzcc@163.com>
2017-06-23 21:27:59 +0800

    修改about页面为即将放映的电影，并添加gif图片

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http }     from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+// import { parseString } from 'xml2js'
+import { DetailPage } from '../detail/detail';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +13,29 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
 
+   films:Observable<any>;
+  public posts: Object[] = [];
+
+  constructor(
+    public navCtrl: NavController,
+    public http: Http,
+  ) {
+
+  }
+
+  ngOnInit() {
+    // this.initBuptBbsRss()
+
+    this.films = this.http.get('https://api.douban.com/v2/movie/coming_soon')
+      .map(res => res.json());
+    // this.fft=this.http.get('https://api.douban.com/v2/movie/subject/1292052').subscribe(res=>res.json());
+
+    // console.log("hahhh:"+JSON.stringify(this.fft))
+  }
+
+  openDetails(film) {
+    this.navCtrl.push(DetailPage, {film: film});
   }
 
 }
